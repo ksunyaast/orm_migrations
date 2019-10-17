961d4e29d5a54c4e6d596c0e6148197733c000ad
Kseniya Astanaeva <ksunyaast@mail.ru>
2019-10-17 22:42:53 +0700

    add some changes

--- school/views.py
+++ school/views.py
@@ -7,13 +7,17 @@ from .models import Student
 def students_list(request):
     template = 'school/students_list.html'
     ordering = 'group'
-    students = Student.objects.order_by(ordering).prefetch_related('teacher')
+    students = Student.objects.order_by(ordering).prefetch_related('teacher').values('name', 'group', 'teacher__name', 'teacher__subject')
     object_list = []
+    students_counter = []
     for s in students:
-        teachers = []
-        for t in s.teacher.all():
-            teachers.append({'name': t.name, 'subject': t.subject})
-        object_list.append({'name': s.name, 'group': s.group, 'teachers': teachers})
+        if s['name'] not in students_counter:
+            students_counter.append(s['name'])
+            object_list.append({'name': s['name'], 'group': s['group'], 'teachers': [{'name': s['teacher__name'], 'subject': s['teacher__subject']}]})
+        else:
+            for o in object_list:
+                if o['name'] == s['name']:
+                    o['teachers'].append({'name': s['teacher__name'], 'subject': s['teacher__subject']})
     context = {
         'object_list': object_list
     }
